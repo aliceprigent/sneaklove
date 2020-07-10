c3436d0a8f791291386415936ff7d1dd68a772f5
Alice Prigent <aliprigent@gmail.com>
2020-07-10 12:05:08 +0200

    v2

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -5,6 +5,11 @@ const Schema = mongoose.Schema;
 const sneakerSchema = new Schema({
     name: String,
     ref: String,
+    picture: {
+      type: String,
+      default:
+        "https://media.gettyimages.com/vectors/default-male-and-female-avatar-profile-picture-icon-grey-man-and-vector-id653036106?b=1&k=6&m=653036106&s=612x612&w=0&h=V22fSP9Jn_EUL3wi-xFuUsLFQ5F6YC3E39gDIaklIyo=",
+    },
     size: Number,
     description: String,
     price: Number,
--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -4,18 +4,30 @@ const SneakerModel = require("../models/Sneaker");
 const uploader = require("./../config/cloudinary");
 
 router.get("/prod-manage", (req, res) => {
-    res.render("products_manage")
+  SneakerModel.find()
+    .then((dbRes) => res.render("products_manage", { sneakers: dbRes }))
+    .catch((err) => console.error(err));
 });
 
 router.get("/prod-add", (req, res) => {
     res.render("products_add")
 });
 
-router.post("/prod-add", uploader.single("picture"), (req, res) => {
-    SneakerModel.create(req.body)
-    console.log(req.body)
-    .then((dbRest) => res.redirect("/prod-manage"))
-    .catch((dbErr) => res.send("Error"));
+
+router.post("/prod-add", uploader.single("picture"), (req, res, next) => {
+  const { name, ref, size, description, price, category, id_tags } = req.body;
+  SneakerModel.create({
+    name,
+    ref,
+    picture: req.file ? req.file.path : undefined,
+    size,
+    description,
+    price,
+    category,
+    id_tags,
+  })
+    .then(() => res.redirect("/prod-manage"))
+    .catch(next);
 });
 
 router.get("/prod-delete/:id", async (req, res, next) => {
@@ -30,21 +42,21 @@ router.get("/prod-delete/:id", async (req, res, next) => {
 router.get("/prod-edit/:id", (req, res, next) => {
     SneakerModel
     .findById(req.params.id) 
-    .then(label => {
-      res.render("/prod-manage", {
-        label
+    .then(sneakers => {
+      res.render("product_edit", {
+        sneakers
       });
     })
     .catch(next); 
 });
 
-router.post("/prod-update/:id", async (req, res) => {
-    try {
-      await SneakerModel.findByIdAndUpdate(req.params.id, req.body);
-      res.redirect("/prod-manage");
-    } catch(err) {
-      next(err);
-    }
+router.post("/prod-edit/:id", (req, res, next) => {
+    SneakerModel
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then(dbRes => {
+    res.redirect("/prod-manage");
+    })
+    .catch(next);
   });
 
 module.exports = router;
